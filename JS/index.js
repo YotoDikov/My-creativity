fa18dfaf4069779df7d9fdae69d4e421ee88358c
Yoto Dikov <yotodikov@gmail.com>
2022-02-13 21:39:51 +0200

    A little bit cleaning the CSS of the navigator buttnos.

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,16 @@
-const buttonsSections = document.getElementsByClassName("link");
+const buttonsSections = document.querySelectorAll("[data-link]");
+// console.log(buttonsSections);
 
 let currentSelection;
 
 for (const item of buttonsSections) {
+  item.dataset.link;
+  console.log(item.dataset.link);
+
+  if (item.dataset.link === "about") {
+    console.log("ABOUT");
+  }
+
   item.addEventListener("click", function (e) {
     e.preventDefault();
 
